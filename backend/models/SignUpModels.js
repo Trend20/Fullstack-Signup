df8f3704f8ecf530ddbb209ba5c5e4e4233cca8a
DevEnock <enockomondi305@gmail.com>
2021-12-08 04:50:16 +0300

    adding the database schema

---
+++ backend/models/SignUpModels.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// define the schema
+const SignUpTemplate = new mongoose.Schema({
+  fullName:{
+    type: String,
+    required: true
+  },
+  username:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  date:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = SignUpTemplate;
