2a811274b3c6bbff9d4eedc7a4fb0a10da55b520
DevEnock <enockomondi305@gmail.com>
2021-12-08 16:01:28 +0300

    updating the backend of the application

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const routesUrl = require('./routes/routes');
+const cors = require('cors');
+
+// activate dotenv
+dotenv.config();
+
+
+// connect serve to the database
+mongoose.connect(process.env.DATABASE_ACCESS, () =>{
+  console.log('Database connected');
+});
 
 const PORT = process.env.PORT || 5000;
 
+
+app.use(express.json());
+app.use(cors());
+// use the rotes as middleware
+app.use('/app', routesUrl);
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`);
 })
